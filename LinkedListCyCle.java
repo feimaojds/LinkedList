d10628cff26af4cb9cbb8aaee5052f6060455218
feimaojds <313061793@qq.com>
2017-09-11 16:02:20 +0800

    Given a linked list, determine if it has a cycle in it.
    思路：如果有环，则一定不会有终点（null）。
         设定两个指针，一个快一个慢，快的每次两步，慢的一次一步，快的每次在环里能追慢的一步，最终总会相逢

---
+++ LinkedListCyCle.java
@@ -0,0 +1,26 @@
+package leecode;
+
+public class LinkedListCyCle {
+
+	
+public boolean hasCycle(ListNode head) {
+	    if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next !=null && fast.next.next !=null){
+        	slow = slow.next;
+        	fast = fast.next.next;
+        	if(slow == fast) return true;
+        }
+        return false;
+    }
+
+class ListNode {
+	      int val;
+	     ListNode next;
+	      ListNode(int x) {
+	          val = x;
+	          next = null;
+	      }
+	  }
+}
