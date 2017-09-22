6ed7453d4c50a638b860f54a2650fe104ecae2c0
feimaojds <313061793@qq.com>
2017-09-22 15:53:04 +0800

    You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
    另外一种方法：
    用两个堆栈，分别放两组数字，后进先出，低位在栈顶。
    每次取栈顶，相加，求进位，相加和/10赋予新创建的结果节点，转移至前一个节点，不断求和、进位，一直到两个栈都是空。若栈是空且cur！=0 ，直接返回该节点。

--- AddTwoNumbersII.java
+++ AddTwoNumbersII.java
@@ -1,42 +1,39 @@
 package leecode;
 
+import java.util.Stack;
+
 public class AddTwoNumbersII {
 	
 	
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l1r = reverseNode(l1);
-        ListNode l2r = reverseNode(l2);
-        ListNode sum = new ListNode(0);
-        ListNode reverseNode = sum;
-        int cur = 0;
-        while(l1r!=null || l2r!=null){
-        	sum.val = (( l1r==null? 0 : l1r.val )+(l2r==null? 0:l2r.val)+cur)%10;
-        	cur = (( l1r==null? 0 : l1r.val )+(l2r==null? 0:l2r.val)+cur)/10;
-        	if(l1r !=null ) l1r = l1r.next;
-        	if(l2r !=null )l2r = l2r.next;
-            if(l1r !=null || l2r!=null){
-            sum.next = new ListNode(0);
-                sum = sum.next;
-            }
-        }
-        if(cur != 0){
-        	sum.next = new ListNode(1);
-                sum = sum.next;
-        } 
-        return reverseNode(reverseNode);
-    }
+	 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	       Stack<ListNode> stack1 = new Stack<ListNode>();
+	        Stack<ListNode> stack2 = new Stack<ListNode>();
+	        while(l1 !=null) {
+	        	stack1.push(l1);
+	        	l1 = l1.next;
+	        }
+	        while(l2 !=null){
+	        	stack2.push(l2);
+	        	l2 = l2.next;
+	        }
+	        int cur = 0;
+	        ListNode reverse = new ListNode(0);
+	        while(!stack1.isEmpty() || !stack2.isEmpty()){
+	        	int sum = ((stack1.isEmpty()? 0:stack1.peek().val) + (stack2.isEmpty()? 0:stack2.peek().val)+cur)%10;
+	        	cur = ((stack1.isEmpty()? 0:stack1.pop().val) + (stack2.isEmpty()? 0:stack2.pop().val)+cur)/10;
+	            reverse.val = sum;
+	            ListNode head = new ListNode(0);
+	            head.next = reverse;
+	            reverse = head;
+	            if(cur !=0 && stack1.isEmpty() && stack2.isEmpty()) {
+	            	reverse.val = 1;
+	                return reverse;
+	            }
+	        }
+	        
+	        return reverse.next;
+	    }
 	
-	public static ListNode reverseNode(ListNode head){
-		if(head == null) return head;
-		ListNode newNode = null;
-		while(head!=null){
-			ListNode next = head.next;
-			head.next = newNode;
-			newNode = head;
-			head = next;
-		}
-		return newNode;
-	}
 	
 	class ListNode {
 		      int val;
